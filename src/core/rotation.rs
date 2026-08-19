a0bdc876762ef8b7a55545487a32872c295d379f
Ali-Shahmansouri <sctshahali@gmail.com>
2026-08-19 12:19:22 +0330

    implement rotation enum

---
+++ src/core/rotation.rs
@@ -0,0 +1,21 @@
+pub enum Rotation {
+    Zero,
+    Ninety,
+    OneEighty,
+    TwoSeventy,
+}
+
+impl Rotation {
+    pub fn default() -> Self {
+        Self::Zero
+    }
+
+    pub fn clockwise(self) -> Self {
+        match self {
+            Self::Zero => Self::Ninety,
+            Self::Ninety => Self::OneEighty,
+            Self::OneEighty => Self::TwoSeventy,
+            Self::TwoSeventy => Self::Zero,
+        }
+    }
+}
