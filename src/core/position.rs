b0e3fca68a6c23144626d839468262fe77ab4025
Ali-Shahmansouri <sctshahali@gmail.com>
2026-08-19 11:26:15 +0330

    implement add and sub with AddSign and SubSign for Position

--- src/core/position.rs
+++ src/core/position.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
+#[derive(Debug, Default, Clone, Copy)]
 pub struct Position {
     x: usize,
     y: usize,
@@ -21,5 +23,41 @@ impl Position {
 impl From<(usize, usize)> for Position {
     fn from((x, y): (usize, usize)) -> Self {
         Position { x, y }
+    }
+}
+
+impl Add<(usize, usize)> for Position {
+    type Output = Self;
+
+    fn add(self, (x, y): (usize, usize)) -> Self::Output {
+        Self {
+            x: self.x.saturating_add(x),
+            y: self.y.saturating_add(y),
+        }
+    }
+}
+
+impl AddAssign<(usize, usize)> for Position {
+    fn add_assign(&mut self, (x, y): (usize, usize)) {
+        self.x = self.x.saturating_add(x);
+        self.y = self.y.saturating_add(y);
+    }
+}
+
+impl Sub<(usize, usize)> for Position {
+    type Output = Self;
+
+    fn sub(self, (x, y): (usize, usize)) -> Self::Output {
+        Self {
+            x: self.x.saturating_sub(x),
+            y: self.y.saturating_sub(y),
+        }
+    }
+}
+
+impl SubAssign<(usize, usize)> for Position {
+    fn sub_assign(&mut self, (x, y): (usize, usize)) {
+        self.x = self.x.saturating_sub(x);
+        self.y = self.y.saturating_sub(y);
     }
 }
